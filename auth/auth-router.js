dd166a0abc9807cb400cb3e236fb2a4784991228
Shawn McManus <xsvfan76@gmail.com>
2019-09-20 11:14:12 -0600

    server is running

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,48 @@
 const router = require('express').Router();
+const Users = require('./auth-model.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let { username, password } = req.body;
+
+  const hash = bcrypt.hashSync(password, 14);
+
+  Users.add({ username, password: hash })
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // token goes here
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'You cannot pass!' });
+      }
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
